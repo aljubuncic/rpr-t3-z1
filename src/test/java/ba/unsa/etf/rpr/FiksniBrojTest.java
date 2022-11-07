e4fe921c62cb20aa6b698d1a5d19a8261085c5a2
Ahmed Ljubunčić <116281740+aljubuncic@users.noreply.github.com>
2022-11-07 13:13:45 +0100

    Update FiksniBrojTest.java

--- src/test/java/ba/unsa/etf/rpr/FiksniBrojTest.java
+++ src/test/java/ba/unsa/etf/rpr/FiksniBrojTest.java
@@ -5,16 +5,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FiksniBrojTest {
-
+    
     @Test
     void ispisi() {
-        FiksniBroj f= new FiksniBroj(Grad.BIHAC,"567890");
-        assertEquals("037/567-890",f.ispisi());
+        FiksniBroj f= new FiksniBroj("567890",Grad.SARAJEVO);
+        assertEquals("033/567-890",f.ispisi());
     }
 
     @Test
     void testHashCode() {
-        FiksniBroj f= new FiksniBroj(Grad.SARAJEVO,"567890");
-        assertEquals(33567890,f.hashCode());
+        FiksniBroj f= new FiksniBroj("567890",Grad.BIHAC);
+        assertEquals(37567890,f.hashCode());
     }
 }
